4fce3b56585145df3f0f58233b7a66dc802f2cbe
tactition <zahidnazir330@gmail.com>
2026-09-01 12:35:22 +0530

    Update mobile header call button size to 46px and add pulsing glow animation

--- header.php
+++ header.php
@@ -228,9 +228,9 @@
                             <div class="mobile-call-btn">
                                 <?php $contact_phone = get_theme_mod('contact_phone', '9622917697'); ?>
                                 <a href="tel:<?php echo esc_attr(str_replace(' ', '', $contact_phone)); ?>"
-                                    class="btn btn-primary rounded-circle p-2"
-                                    style="width: 40px; height: 40px; display: inline-flex; align-items: center; justify-content: center; background-color: #3860D2; border-color: #3860D2;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill text-white" viewBox="0 0 16 16">
+                                    class="mobile-call-pulse"
+                                    aria-label="Call Now">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-telephone-fill text-white" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
                                     </svg>
                                 </a>
